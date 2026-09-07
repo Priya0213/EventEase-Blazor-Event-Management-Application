7754edccee64ffea724266013a0c38142804997c
Priyanka Kachhap <priyanka.kmri0369@gmail.com>
2026-09-07 16:36:14 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,14 @@
+using EventEase;
+using EventEase.Services;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+// State management service
+builder.Services.AddScoped<UserSessionService>();
+
+await builder.Build().RunAsync();
